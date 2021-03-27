7aeb8c91fd0be3190e395aebad759494c22fb534
shubham sapkal <66507418+Realocity@users.noreply.github.com>
2021-03-27 20:12:43 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,121 @@
+<?php
+require_once('config.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Registration | PHP</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+
+<div>
+	<?php
+	
+	?>	
+</div>
+
+<div>
+	<form action="registration.php" method="post">
+		<div class="container">
+			
+			<div class="row">
+				<div class="col-sm-3">
+					<h1>Registration</h1>
+					<p>Fill up the form with correct values.</p>
+					<hr class="mb-3">
+					<label for="firstname"><b>First Name</b></label>
+					<input class="form-control" id="firstname" type="text" name="firstname" required>
+
+					<label for="lastname"><b>Last Name</b></label>
+					<input class="form-control" id="lastname"  type="text" name="lastname" required>
+
+					<label for="email"><b>Email Address</b></label>
+					<input class="form-control" id="email"  type="email" name="email" required>
+
+					<label for="areacode"><b>Area Code</b></label>
+					<input class="form-control" id="areacode"  type="text" name="areacode" required>
+
+					<label for="phonenumber"><b>Phone Number</b></label>
+					<input class="form-control" id="phonenumber"  type="text" name="phonenumber" required>
+
+					<label for="address"><b>Address</b></label>
+					<input class="form-control" id="address"  type="text" name="address" required>
+
+					<label for="password"><b>Password</b></label>
+					<input class="form-control" id="password"  type="password" name="password" required>
+
+					<label for="confirmpassword"><b>Confirm Password</b></label>
+					<input class="form-control" id="confirmpassword"  type="password" name="confirmpassword" required>
+
+					<label for="gender"><b>Gender</b></label>
+					<input class="form-control" id="gender"  type="text" name="gender" required>
+					<hr class="mb-3">
+					<input class="btn btn-primary" type="submit" id="register" name="create" value="Sign Up">
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script type="text/javascript">
+	$(function(){
+		$('#register').click(function(e){
+
+			var valid = this.form.checkValidity();
+
+			if(valid){
+
+
+			var firstname 	= $('#firstname').val();
+			var lastname	= $('#lastname').val();
+			var email 		= $('#email').val();
+			var areacode 		= $('#areacode').val();
+			var phonenumber = $('#phonenumber').val();
+			var address 		= $('#address').val();
+			var password 	= $('#password').val();
+			var confirmpassword 	= $('#confirmpassword').val();
+			var gender 		= $('#gender').val();
+			
+
+				e.preventDefault();	
+
+				$.ajax({
+					type: 'POST',
+					url: 'process.php',
+					data: {firstname: firstname,lastname: lastname,email: email,areacode: areacode,phonenumber: phonenumber,address: address,password: password,confirmpassword: confirmpassword,gender: gender},
+					success: function(data){
+					Swal.fire({
+								'title': 'Successful',
+								'text': data,
+								'type': 'success'
+								})
+							
+					},
+					error: function(data){
+						Swal.fire({
+								'title': 'Errors',
+								'text': 'There were errors while saving the data.',
+								'type': 'error'
+								})
+					}
+				});
+
+				
+			}else{
+				
+			}
+
+			
+
+
+
+		});		
+
+		
+	});
+	
+</script>
+</body>
+</html>
